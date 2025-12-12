ff59ead8153b6c2ae0a9815c2a4000a12b375306
Prasham Marfatia <marfatia.p@northeastern.edu>
2025-12-12 11:02:15 -0500

    Register llm_bel in statement presentation ordering for HTML output

--- indra/util/statement_presentation.py
+++ indra/util/statement_presentation.py
@@ -125,7 +125,7 @@ db_sources = ['psp', 'cbn', 'pc', 'bel_lc', 'signor', 'biogrid',
 """Database source names as they appear in the DB"""
 
 reader_sources = ['geneways', 'tees', 'gnbr', 'semrep', 'isi', 'trips',
-                  'rlimsp', 'medscan', 'eidos', 'sparser', 'reach']
+                  'rlimsp', 'medscan', 'eidos', 'sparser', 'reach', 'llm_bel']
 """Reader source names as they appear in the DB"""
 
 # These are mappings where the actual INDRA source, as it appears
