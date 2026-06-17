28eeca96347016495272baae4eed02fbe3c2682f
Ben Gyori <ben.gyori@gmail.com>
2026-06-16 22:41:29 -0400

    Refine pysb install

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ with open(readme_path, 'r', encoding='utf-8') as fh:
 
 
 def main():
-    install_list = ['pysb @ git+https://github.com/pysb/pysb.git', 'objectpath',
+    install_list = ['pysb @ git+https://github.com/pysb/pysb.git; '
+                    'python_version >= "3.10"',
+                    'pysb<=1.16.0; python_version < "3.10"',
+                    'objectpath',
                     'requests>=2.11', 'lxml', 'ipython', 'future',
                     'networkx>=3', 'pandas>=2', 'ndex2', 'jinja2',
                     'protmapper>=0.0.29', 'obonet',
